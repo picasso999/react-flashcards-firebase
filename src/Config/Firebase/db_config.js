59b887cc2777b920dc2ef09612417973987243f1
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2017-11-07 23:33:19 +0530

    this is reac-firebase-flashcrds-basic-app

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    
+    apiKey: "AIzaSyD2BuJaQYWzeFc1YUh5KvOADh9HmgZXCxg",
+    authDomain: "react-flashcards-999.firebaseapp.com",
+    databaseURL: "https://react-flashcards-999.firebaseio.com",
+    projectId: "react-flashcards-999",
+    storageBucket: "",
+    messagingSenderId: "23233993739"
+}
